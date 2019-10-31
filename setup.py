a642ffa57792a3ac0584159b276bff447a0e5903
Kamil Glod <kamilkarolglod@gmail.com>
2019-10-31 17:49:35 +0100

    Add mypy

--- setup.py
+++ setup.py
@@ -1,24 +1,3 @@
-from setuptools import setup, find_packages
-from pg_batch_inserter import __version__ as version
+from setuptools import setup
 
-
-setup(
-    name='pg_batch_inserter',
-    version=version,
-    description='RTB apps PostgreSQL Batch Inserter',
-    url='git@github.com:rtbhouse-apps/rtb-apps-pylib.git',
-    author='rtb apps team',
-    author_email='apps@rtbhouse.com',
-
-    packages=find_packages(exclude=['tests']),
-    install_requires=[
-    ],
-    extras_require={
-        'dev': [
-            'pytest==5.2.0',
-        ],
-        'test': [
-            'psycopg2-binary==2.8.4'
-        ],
-    },
-)
+setup()
