3c88c5679ad3801b9c658e3090bb51b78c4e2044
Kamil Glod <kamilkarolglod@gmail.com>
2019-11-06 10:06:03 +0100

    Add default PG dsn

--- tests/e2e/conftest.py
+++ tests/e2e/conftest.py
@@ -5,7 +5,8 @@ import pytest
 
 @pytest.fixture
 def psycopg_cursor():
-    psycopg2_connection = psycopg2.connect(environ['DB_DSN'])
+    db_dsn = environ.get('DB_DSN', 'postgresql://postgres@localhost:5432/e2e_db')
+    psycopg2_connection = psycopg2.connect(db_dsn)
     psycopg2_cursor = psycopg2_connection.cursor()
 
     yield psycopg2_cursor
