04e0acf12996ab0bb6c6f9b69b6b92bdf401ff83
Jacek Jabłoski <jacek.jablonski@gmail.com>
2022-08-12 16:09:57 +0200

    fix

--- bin/test.sh
+++ bin/test.sh
@@ -5,4 +5,4 @@ set -o pipefail
 
 cd "`dirname $0`/.."
 
-docker compose run -it --rm --service-ports py sh -c "/wait && poetry run pytest"
+docker compose run -it --rm --service-ports -e WAIT_HOSTS=db:5432 py sh -c "/wait && poetry run pytest"
